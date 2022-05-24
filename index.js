a9ae86a5c7396c9a86a2021ac93cc497f12fc306
Waqas-Ahmed1998 <nizamaniwaqas4@gmail.com>
2022-05-25 03:00:59 +0500

    second commit

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 $(document).ready(function () {
   $("#step-2").click(function () {
-    $("#video").toggle();
-    $("#email-input").toggle();
+    $("#video").show();
+    $("#email-input").hide();
+    $("#disclaimer").hide();
+    $("#headline").text(
+      "Watch This Video Below To Discover What Your Next Step Is"
+    );
+    $("#sub-headline").text("Thank you for subscribing");
   });
 });
